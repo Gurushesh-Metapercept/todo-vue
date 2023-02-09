e47ad4f5a23b4d45ea7dd4da3dec2a7ff830efca
Gurushesh <gurushesh.p@metapercept.com>
2023-02-09 18:34:27 +0530

    firebase integration to add todo in database

--- src/views/TaskView.vue
+++ src/views/TaskView.vue
@@ -1,67 +1,125 @@
 <template>
-  <v-app id="tasl">
+  <v-app>
     <Header />
-    <AddTodo />
-    <div class="boxx">
-      <Card :todos="todos" />
+    <!-- <AddTodo /> -->
+    <div class="wrapper">
+      <v-card class="mx-auto py-10" max-width="800">
+        <v-row class="d-flex mx-auto justify-center align-center">
+          <v-col cols="4">
+            <v-text-field
+              label="Title"
+              :rules="rules"
+              hide-details="auto"
+              class="mb-2"
+              v-model="newTodo"
+              @keyup.enter="addTodo"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="4">
+            <v-text-field
+              label="Task"
+              :rules="rules"
+              hide-details="auto"
+              class="mb-2"
+              v-model="newTask"
+              @keyup.enter="addTodo"
+            ></v-text-field>
+          </v-col>
+          <v-col cols="1">
+            <v-btn color="indigo" class="ml-2 custom_btn" @click="addTodo">
+              Add
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-card>
     </div>
+    <v-row v-if="err_show" class="custom_error">
+      <v-col cols="3" class="mx-auto">
+        <v-alert dense outlined type="error">
+          {{ this.err_message }}
+        </v-alert>
+      </v-col>
+    </v-row>
+    <v-row v-if="success_show" class="custom_error">
+      <v-col cols="3" class="mx-auto">
+        <v-alert dense text type="success">
+          {{ this.err_message }}
+        </v-alert>
+      </v-col>
+    </v-row>
+
+    <v-card class="mx-auto d-flex justify-center flex-wrap" flat>
+      <Card :todosList="todosList" />
+    </v-card>
   </v-app>
 </template>
 
 <script>
 import Header from "@/components/Header.vue";
 import Card from "../components/Card.vue";
-import AddTodo from "@/components/AddTodo.vue";
+
+import { db } from "../firebase/init";
+import { collection, addDoc, getDocs } from "firebase/firestore";
+
 export default {
   components: {
     Header,
     Card,
-    AddTodo,
   },
   data() {
     return {
-      todos: [
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time",
-        },
-        {
-          title: "ths is tiele",
-          task: "thsi is task and you have to complete within time thsi is task and you have to complete within time",
-        },
-      ],
+      todosList: [],
+      newTodo: "",
+      newTask: "",
+      err_show: false,
+      success_show: false,
+      err_message: "",
     };
+  },
+  methods: {
+    async addTodo() {
+      console.log("cked");
+      if (this.newTodo != "" && this.newTask != "") {
+        // this.todosList.push({ title: this.newTodo, task: this.newTask });
+        await addDoc(collection(db, "todos"), {
+          title: this.newTodo,
+          task: this.newTask,
+        });
+        this.newTodo = "";
+        this.newTask = "";
+        this.err_message = "New todo added";
+        this.err_show = false;
+        this.success_show = true;
+      } else if (
+        (this.newTodo != "" && this.newTask == "") ||
+        (this.newTodo == "" && this.newTask != "") ||
+        (this.newTask == "" && this.newTodo == "")
+      ) {
+        this.err_message = "please fill all the fields";
+        this.success_show = false;
+        this.err_show = true;
+      }
+    },
+  },
+  async mounted() {
+    const colRef = collection(db, "todos");
+    const docsSnap = await getDocs(colRef);
+    console.log("from mounted");
+    // console.log(docsSnap.docs);
+    this.todosList = docsSnap.docs;
+    docsSnap.forEach((doc) => {
+      console.log(doc.data());
+    });
   },
 };
 </script>
 
 <style>
-.boxx {
-  display: flex;
-  justify-content: center;
-  max-width: 100%;
-  margin: 0 auto;
-  flex-wrap: wrap;
-  margin-top: 2rem;
+.wrapper {
+  margin: 6rem;
+}
+.custom_error {
+  margin-top: -5rem;
+  max-height: 100px;
 }
 </style>
